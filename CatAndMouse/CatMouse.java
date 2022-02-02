0cb4b3a381612e1070ec79cdb97068932e3cf906
wfrice <ricewilliam125@gmail.com>
2022-02-02 08:51:08 -0500

    added third maze

--- CatAndMouse/CatMouse.java
+++ CatAndMouse/CatMouse.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
+/**
+ * Class that describes the
+ */
 public class CatMouse {
 	
 	private ArrayList<char[]> maze = new ArrayList<char[]>();
-	private int[] cat;
+	private int cat;
 
 	public CatMouse() throws IOException {
 
@@ -17,19 +19,29 @@ public class CatMouse {
 		FileReader readFile = new FileReader(filename);
 		BufferedReader inFile = new BufferedReader(readFile);
 		String inputString = inFile.readLine();
+		int maxLength = inputString.length();
 		int row = 0;
 		while (inputString != null){
 
-			System.out.println(inputString);
-			for (int y = 0; y < inputString.length(); y++) {
-				if (inputString.charAt(y) == 'C') {
-					this.cat = new int[] {row, y};
+			if (inputString.length() == 0) {
+				continue;
+			}
+			else if (inputString.length() < maxLength) {
+				for (int i = inputString.length(); i < maxLength; i++) {
+					inputString += " ";
 				}
 			}
-			maze.add(inputString.toCharArray());
+			else {
+				for (int y = 0; y < inputString.length(); y++) {
+					if (inputString.charAt(y) == 'C') {
+						this.cat = y;
+					}
+				}
+				this.maze.add(inputString.toCharArray());
 
-			inputString = inFile.readLine();
-			row++;
+				inputString = inFile.readLine();
+				row++;
+			}
 		}
 
 		inFile.close();
@@ -41,18 +53,16 @@ public class CatMouse {
 		FileReader readFile = new FileReader(filename);
 		BufferedReader inFile = new BufferedReader(readFile);
 		String inputString = inFile.readLine();
-		int row = 0;
 		while (inputString != null){
 
 			for (int y = 0; y < inputString.length(); y++) {
 				if (inputString.charAt(y) == 'C') {
-					this.cat = new int[] {row, y};
+					this.cat = y;
 				}
 			}
-			maze.add(inputString.toCharArray());
+			this.maze.add(inputString.toCharArray());
 
 			inputString = inFile.readLine();
-			row++;
 		}
 
 		inFile.close();
@@ -60,9 +70,9 @@ public class CatMouse {
 	}
 
 	public String findMouseRunner() {
-		boolean found = findMouse(this.cat[0], this.cat[1]);
+		boolean found = findMouse(0, this.cat);
 
-		System.out.println("The Cat "  + (found == true ? "found" : "didn't find") + " the mouse in this " + maze.size() + "x" + maze.get(0).length + " maze as follows:");
+		System.out.println("The Cat "  + (found == true ? "found" : "didn't find") + " the mouse in this " + this.maze.size() + "x" + this.maze.get(0).length + " maze as follows:");
 		return this.toString();
 	}
 
@@ -70,24 +80,24 @@ public class CatMouse {
 
 		char tail = '0';
 
-		if (x < 0 || x >= maze.size() || y < 0 || y >= maze.get(x).length) {
+		if (x < 0 || x >= this.maze.size() || y < 0 || y >= this.maze.get(x).length) {
 			return false;
 		}
 
-		if (maze.get(x)[y] == 'M') {
+		if (this.maze.get(x)[y] == 'M') {
 			return true;
 		}
-		else if (maze.get(x)[y] == tail || maze.get(x)[y] == '#') {
+		else if (this.maze.get(x)[y] == tail || this.maze.get(x)[y] == '#') {
 			return false;
 		}
 
-		if (maze.get(x)[y] != 'C') {
-			maze.get(x)[y] = tail;
+		if (this.maze.get(x)[y] != 'C') {
+			this.maze.get(x)[y] = tail;
 		}
 
 		if (!(findMouse(x-1, y) || findMouse(x, y+1) || findMouse(x+1, y) ||  findMouse(x, y-1))) {
-			if (maze.get(x)[y] != 'C') {
-				maze.get(x)[y] = ' ';
+			if (this.maze.get(x)[y] != 'C') {
+				this.maze.get(x)[y] = ' ';
 			}
 		}
 		else {
@@ -101,9 +111,9 @@ public class CatMouse {
 	public String toString() {
 
 		String mazeString = "";
-		for (int i = 0; i < maze.size(); i++) {
-			for (int j = 0; j < maze.get(0).length; j++) {
-				mazeString += maze.get(i)[j];
+		for (int i = 0; i < this.maze.size(); i++) {
+			for (int j = 0; j < this.maze.get(0).length; j++) {
+				mazeString += this.maze.get(i)[j];
 			}
 			mazeString += "\n";
 		}
